1a90690ac252ad8cddf22358f981e885012947e4
Çağdaş Yarıcı <cagdasyarici16@gmail.com>
2023-03-25 02:11:01 +0300

    Database CodeFirst

---
+++ DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete
+{
+    internal class Context:DbContext
+    {
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Content> Contents { get; set; }
+        public DbSet<Heading> Headings { get; set; }
+        public DbSet<Writer> Writers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CHADO\\MSSQLKD14;Database=NLayerMVCProject;Trusted_Connection=True");
+        }
+    }
+}
